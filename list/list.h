35ab4b194caaed3ecd2e9461c6c519d1411abc87
brianSalk <brian.salkas@gmail.com>
2021-08-04 13:18:28 -0500

    added aliases to list, also added forward decls of iterators

--- list/list.h
+++ list/list.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <cstddef>
+#include <memory>
 
 namespace brian {
+template <typename T, typename Allocator = std::allocator<T>>
+class list {
+	template <bool Is_Const>
+	class __base_iterator;
+	template <bool Is_Const>
+	class list_iterator : public __base_iterator<Is_Const>;
+	template <bool Is_Const>
+	class reverse_list_iterator : public __base_iterator<Is_Const>;
+	public:
+	using value_type = T;
+	using allocator_type = Allocator;
+	using size_type  = std::size_t;
+	using difference_type = std::ptrdiff_t;
+	using referene = value_type&;
+	using const_reference = value_type const&;
+	using pointer = typename std::allocator_traits<Allocator>::pointer;
+	using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
+	using iterator = list_iterator<false>;
+	using const_iterator = list_iterator<true>;
+	using reverse_iterator = reverse_list_iterator<false>;
+	using const_reverse_iterator = reverse_list_iterator<true>;
+
+
 	
-}
+};// END CLASS LIST
+}// END NAMESPACE BRIAN
