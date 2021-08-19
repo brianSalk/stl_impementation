36a99b5fd3d6a6a44bbac46f513cc10c871a62c1
brianSalk <brian.salkas@gmail.com>
2021-08-19 01:14:21 -0500

    began impelmenting the iterator and the constructors

--- vector/vector.h
+++ vector/vector.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstddef>
+#include <initializer_list>
 #include <iostream>
 #include <iterator>
 #include <memory>
@@ -29,30 +30,88 @@ private:
 	using Traits = std::allocator_traits<allocator_type>;
 	size_type capacity;
 	size_type n;
+	// TO DO: change the constructors so they accept a pointer to arr instead of size_t
 	template <bool Is_Const>
 	class vector_iterator {
-		// VIDPR
+		// VIDPR;
+		friend vector<T,Allocator>;
+	public:
 		using value_type = T;
 		using iterator_category = std::contiguous_iterator_tag;
 		using difference_type = std::ptrdiff_t;
-		using pointer = std::conditional_t<Is_Const,vector::const_pointer, vector::pointer>;
-		using reference = std::conditional<Is_Const,vector::const_reference, vector::reference>;
+		using pointer = typename std::conditional_t<Is_Const,vector::const_pointer, vector::pointer>;
+		using reference = typename std::conditional_t<Is_Const,vector::const_reference, vector::reference>;
 		vector_iterator() = default;
-		vector_iterator(size_t i) : itr_i(i) {}
+		vector_iterator(pointer i) : itr_p(i) {}
 		vector_iterator(vector_iterator const& it) = default;
 		template <bool Was_Const, typename std::enable_if<Is_Const || !Was_Const>::type>
-		vector_iterator(vector_iterator const& it) : itr_i(it.itr_i) {}
-		private:
-		size_t itr_i;
+		vector_iterator(vector_iterator const& it) : itr_p(it.itr_p) {}
 
-
+		vector_iterator operator++() {
+			return vector_iterator(++itr_p);
+		}
+		vector_iterator operator++(int) {
+			return vector_iterator(itr_p++);
+		}
+		vector_iterator operator--() {
+			return vector_iterator(--itr_p);
+		}
+		vector_iterator operator--(int) {
+			return vector_iterator(itr_p--);
+		}
+		friend bool operator==(vector_iterator const& lhs, vector_iterator const& rhs) {
+			return lhs.itr_p == rhs.itr_p;
+		}
+		friend vector_iterator operator +=(vector_iterator& it, size_t n) {
+			return it.itr_p += n;
+		}
+		friend vector_iterator operator -=(vector_iterator& it, size_t n) {
+			return it.itr_p -= n;
+		}
+		friend vector_iterator operator +(vector_iterator const& it, size_t n) {
+			return it.itr_p + n;
+		}
+		friend vector_iterator operator -(vector_iterator const& it, size_t n) {
+			return it.itr_p - n;
+		}
+		auto operator<=>(vector_iterator const& i) const {
+			return this->itr_p <=> i.itr_p;
+		}
+		reference operator*() {
+			return *itr_p;
+		}
+		
+	private:
+		pointer itr_p;
 	};
 public:
 	// constructors
-	vector();
+	constexpr vector() noexcept(noexcept(Allocator()));
+	constexpr explicit vector(Allocator const& alloc) noexcept;
+	constexpr vector(size_t,T const&, Allocator const& alloc = Allocator());
+	constexpr explicit vector(size_t count, Allocator const& alloc = Allocator());
+	template <typename It, typename std::iterator_traits<It>::pointer=nullptr>
+	constexpr vector(It,It);
+	vector(std::initializer_list<T> il);
 	// observers
+	iterator 				begin() { return iterator(arr); }
+	const_iterator          begin() const { return const_iterator(arr); }
+	const_iterator          cbegin() const { return const_iterator(arr); }
+	const_reverse_iterator  crbegin() const { return const_reverse_iterator(arr + n); }
+	const_reverse_iterator  rbegin() { return reverse_iterator(arr + n); }
+	reverse_iterator 		rbegin() const { return const_reverse_iterator(arr + n); }
+
+	iterator 				end() { return iterator(arr + n); }
+	const_iterator			end() const { return const_iterator(arr + n); } 				
+	const_iterator			cend() const { return const_iterator(arr + n); }
+	const_reverse_iterator  crend() const { return const_reverse_iterator(arr); }
+	const_reverse_iterator  rend() { return const_reverse_iterator(arr); }
+	reverse_iterator 		rend() const { return const_reverse_iterator(arr); }
 	// destructor
 	~vector();
+private:
+	// helpers
+	vector(Allocator const& alloc,size_t s,size_t c) : n(s), capacity(c), arr(Traits::allocate(allocator,arr,c)), allocator(alloc) {}
 }; // END CLASS VECTOR
 } // END NAMESPACE BRIAN
 #include "vector.hpp"
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,12 +1,38 @@
 #pragma once
 #include "vector.h"
-
+#include <initializer_list>
+#include <iterator>
+// constructors
+// default constructor
 namespace brian {
 	template <typename T, typename Allocator>
-	vector<T,Allocator>::vector() {
-		capacity = 1;
-		n = 0;
-		arr = Traits::allocate(allocator,1);
+	constexpr vector<T,Allocator>::vector() noexcept(noexcept(Allocator())) : vector(Allocator(),0,1) {}
+	template <typename T, typename Allocator>
+	constexpr vector<T,Allocator>::vector(Allocator const& alloc) noexcept : vector(alloc,0,1) {}
+	template <typename T, typename Allocator>
+	constexpr vector<T,Allocator>::
+	vector(size_t count, T const& val, Allocator const& alloc) : vector(alloc,count,count){
+		for (size_t i{0}; i < count;++i) {
+			Traits::construct(allocator,arr+i,val);
+		}
+	}
+	template <typename T, typename Allocator>
+	constexpr vector<T,Allocator>::vector(size_t count, Allocator const& alloc) : vector(alloc,count,count) { for (size_t i{0};i<count;++i)Traits::construct(allocator,arr); }
+	template <typename T, typename Allocator>
+	template <typename It, typename std::iterator_traits<It>::pointer>
+	constexpr vector<T,Allocator>::vector(It first, It last) {
+		// insert overload just like list, one overload is for contiguous/random
+		// access the other for forward/bidirectional	
+	}
+	template <typename T, typename Allocator>
+	vector<T, Allocator>::vector(std::initializer_list<T> il) {
+		n = il.size();	
+		capacity = n;
+		arr = Traits::allocate(allocator,n);
+		size_t i = 0;
+		for (auto const& each : il) {
+			Traits::construct(allocator,arr + (i++),each);
+		}
 	}
 	template <typename T, typename Allocator>
 	vector<T,Allocator>::~vector() {
