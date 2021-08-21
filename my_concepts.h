2620ef60ed4f66e6b838b02ca0e5dd7f0d47da31
brianSalk <brian.salkas@gmail.com>
2021-08-21 15:41:26 -0500

    added iterator concepts

--- my_concepts.h
+++ my_concepts.h
@@ -1,7 +1,14 @@
 #include <type_traits>
+#include <iterator>
 namespace brian {
 	template <typename U>
 	concept __less_than_comparable = requires(U a, U b) {
 		{ a < b } -> std::convertible_to<bool>;
 	};
+	template <typename It>
+	concept at_least_random_access_iterator = std::is_same<typename It::iterator_category, std::random_access_iterator_tag>::value || 
+		std::is_same<typename It::iterator_category,std::contiguous_iterator_tag>::value;
+	template <typename It>
+	concept at_most_bidirectional_iterator = !at_least_random_access_iterator<It>;
 }
+
