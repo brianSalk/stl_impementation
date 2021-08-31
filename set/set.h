bb65e24042a1bbaf9aca1bc3c8fd773157011435
brianSalk <brian.salkas@gmail.com>
2021-08-31 17:08:03 -0500

    added set to my stl containers, this one will be very different than the others so far

---
+++ set/set.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <memory>
+namespace brian {
+template <typename Key, typename Compare = std::less<Key>, typename Allocator = std::allocator<Key> >
+class set {
+	using key_type = Key;
+	using value_type = Key;	
+	using size_type = size_t;
+	using difference_type = std::ptrdiff_t;
+	using key_compare = Compare;
+	using value_compare = Compare;
+	using allocator_type = Allocator;
+	using reference = value_type&;
+	using const_reference = value_type const&;
+	using pointer = typename std::allocator_traits<allocator_type>::pointer;
+	using const_pointer = typename std::allocator_traits<allocator_type>::const_pointer;
+	using iterator = typename set_iterator<false>;
+	using const_iterator = typename set_iterator<true>;
+	using reverse_iterator = std::reverse_iterator<iterator>;
+	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+	using node_type = node;
+	using insert_return_type = unsure;
+
+}; // END CLASS SET
+}// END NAMESPACE BRIAN
