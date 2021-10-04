f830ce970edadd8f1f24fe490eb5c8b99b335061
Brian Salkas <brian@pop-os.localdomain>
2021-10-04 02:01:42 -0500

    added the std::to_array function

--- array/array.h
+++ array/array.h
@@ -25,9 +25,9 @@ namespace brian {
 		using reverse_iterator = std::reverse_iterator<iterator>;
 		using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-	//	array() = default;
+		array() = default;
 		~array() = default;
-	//	array& operator=(array const&) = default;
+		array& operator=(array const&) = default;
 		// observers
 		constexpr size_t size() const {return N;}
 		constexpr reference front() { return *arr; }
@@ -36,8 +36,8 @@ namespace brian {
 		constexpr const_reference back() const { return arr[N-1]; }
 		constexpr reference operator[](size_t i) { return arr[i]; }
 		constexpr const_reference operator[](size_t i) const { return arr[i]; }
-		constexpr reference at(size_t i) { if (!(i < N)) { throw std::out_of_range("value out of range"); } return arr[i]; }
-		constexpr const_reference at(size_t i) const { if (!(i < N)) { throw std::out_of_range("value out of range"); } return arr[i]; }
+		constexpr reference at(size_t i) { return (i < N) ? arr[i]: throw std::out_of_range("index out of range");}
+		constexpr const_reference at(size_t i) const {  return at(i);}
 		constexpr pointer data() noexcept { return arr; }
 		constexpr const_pointer data() const noexcept { return arr; }
 		[[nodiscard]]
@@ -45,14 +45,26 @@ namespace brian {
 		constexpr size_t max_size() const noexcept { return N; }
 		// modifiers 
 		constexpr void fill(T const& val);
+		constexpr void swap(array& other) noexcept(std::is_nothrow_swappable_v<T>) {
+			for (size_t i{0}; i < N; ++i) {
+				std::swap(this->arr[i], other.arr[i]);
+			}
+		}
 		// iterator methods
 		constexpr iterator begin() { return arr; }
 		constexpr const_iterator begin() const { return arr; }
 		constexpr const_iterator cbegin() const { return arr; }
+		constexpr reverse_iterator rbegin() { return reverse_iterator(arr+N); }
+		constexpr reverse_iterator rbegin() const { return const_reverse_iterator(arr+N); }
+		constexpr const_reverse_iterator crbegin() const { return const_reverse_iterator(arr+N); }
 
 		constexpr iterator end() { return iterator(arr+N); }
 		constexpr const_iterator end() const { return arr+N; }
 		constexpr const_iterator cend() const { return arr+N; }
+		constexpr reverse_iterator rend() { return reverse_iterator(arr); }
+		constexpr const_reverse_iterator rend() const { return const_reverse_iterator(arr); }
+		constexpr const_reverse_iterator crend() const { return const_reverse_iterator(arr); }
+
 		// non-member functions and friends
 		constexpr friend bool operator==(array const& lhs, array const& rhs) {
 			if (lhs.size() != rhs.size()) { return false; }
@@ -69,5 +81,56 @@ namespace brian {
 		}
 			
 	}; // END CLASS ARRAY
+	template <size_t I, typename T, size_t N> 
+	constexpr T& get(array<T,N> & arr) noexcept {
+		return arr[I];
+	}
+	template <size_t I, typename T, size_t N> 
+	constexpr T&& get(array<T,N> && arr) noexcept {
+		return arr[I];
+	}
+	template <size_t I, typename T, size_t N> 
+	constexpr T const& get(array<T,N> & arr) noexcept {
+		return arr[I];
+	}
+	template <size_t I, typename T, size_t N> 
+	constexpr T const&& get(array<T,N> && arr) noexcept {
+		return arr[I];
+	}
 } // END NAMESPACE BRIAN
+// the following helper classes allow brian::array to be treated like a tuple
+namespace std {
+	template <typename T, size_t N>
+	constexpr brian::array<std::remove_cv_t<T>, N> to_array(T (&arr)[N]) {
+		brian::array<T,N> array;
+		for (size_t i{0}; i < N; ++i) {
+			array[i] = arr[i];
+		}
+		return array;
+	}
+	template <typename T, size_t N>
+	constexpr brian::array<std::remove_cv_t<T>, N> to_array(T (&&arr)[N]) {
+		brian::array<T,N> array;
+		for (size_t i{0}; i < N; ++i) {
+			array[i] = std::move(arr[i]);
+		}
+		return array;
+	}
+	template <typename T>
+	struct tuple_size;
+	template <typename T, size_t N>
+	struct tuple_size<brian::array<T,N> > : std::integral_constant<size_t, N> {
+		static size_t const value = N;	
+		using value_type = size_t;
+		using type = std::integral_constant<value_type,value>;
+		operator size_t () { return value; }
+		size_t operator()() { return value; }
+	};
+	template <size_t I, typename T>
+	struct tuple_element;
+	template <size_t I, typename T, size_t N>
+	struct tuple_element<I, brian::array<T,N>> {
+		using type = T;	
+	};
+} // END NAMESPACE STD
 #include "array.hpp"
